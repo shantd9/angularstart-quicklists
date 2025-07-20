113533a52b5e529bbd20d743fad853b333c6f3bd
Shant Derounian <shantderounian@gmail.com>
2025-07-20 11:11:36 +0200

    Create a Form Modal component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, signal} from '@angular/core';
+import {Component, effect, inject, signal} from '@angular/core';
 import {ModalComponent} from '../shared/ui/modal.component';
 import {Checklist} from '../shared/interfaces/checklist';
 import {FormBuilder} from '@angular/forms';
@@ -38,4 +38,14 @@ export default class HomeComponent {
   checklistForm = this.formBuilder.nonNullable.group({
     title: [''],
   });
+
+  constructor() {
+    effect(() => {
+      const checklist = this.checklistBeingEdited();
+
+      if (!checklist) {
+        this.checklistForm.reset();
+      }
+    });
+  }
 }
