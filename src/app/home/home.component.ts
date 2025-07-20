725171ea39cdf7208e8d0e6234a692c491af1527
Shant Derounian <shantderounian@gmail.com>
2025-07-20 11:24:19 +0200

    Creating a checklist service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import {ModalComponent} from '../shared/ui/modal.component';
 import {Checklist} from '../shared/interfaces/checklist';
 import {FormBuilder} from '@angular/forms';
 import {FormModalComponent} from '../shared/ui/form-modal.component';
+import {ChecklistService} from '../shared/data-access/checklist.service';
 
 @Component({
   selector: 'app-home',
@@ -19,6 +20,7 @@ import {FormModalComponent} from '../shared/ui/form-modal.component';
               : 'Add Checklist'
           "
             [formGroup]="checklistForm"
+            (save)="checklistService.add$.next(checklistForm.getRawValue())"
             (close)="checklistBeingEdited.set(null)"
           />
         </ng-template>
@@ -32,6 +34,7 @@ import {FormModalComponent} from '../shared/ui/form-modal.component';
 })
 export default class HomeComponent {
   formBuilder = inject(FormBuilder);
+  checklistService = inject(ChecklistService);
 
   checklistBeingEdited = signal<Partial<Checklist> | null>(null);
 
---
+++ src/app/shared/data-access/checklist.service.ts
@@ -0,0 +1,58 @@
+import { Injectable, computed, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Subject } from 'rxjs';
+import { AddChecklist, Checklist } from '../interfaces/checklist';
+
+export interface ChecklistsState {
+  checklists: Checklist[];
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ChecklistService {
+  // state
+  private state = signal<ChecklistsState>({
+    checklists: [],
+  });
+
+  // selectors
+  checklists = computed(() => this.state().checklists);
+
+  // sources
+  add$ = new Subject<AddChecklist>();
+
+  constructor() {
+    // reducers
+    this.add$.pipe(takeUntilDestroyed()).subscribe((checklist) =>
+      this.state.update((state) => ({
+        ...state,
+        checklists: [...state.checklists, this.addIdToChecklist(checklist)],
+      }))
+    );
+  }
+
+  private addIdToChecklist(checklist: AddChecklist) {
+    return {
+      ...checklist,
+      id: this.generateSlug(checklist.title),
+    };
+  }
+
+  private generateSlug(title: string) {
+    // NOTE: This is a simplistic slug generator and will not handle things like special characters.
+    let slug = title.toLowerCase().replace(/\\s+/g, '-');
+
+    // Check if the slug already exists
+    const matchingSlugs = this.checklists().find(
+      (checklist) => checklist.id === slug
+    );
+
+    // If the title is already being used, add a string to make the slug unique
+    if (matchingSlugs) {
+      slug = slug + Date.now().toString();
+    }
+
+    return slug;
+  }
+}
